4345367cb0aa11fb34f257d292bff7fa6788bc9c
tuhindewan <tuhinsshadow@gmail.com>
2022-07-16 20:57:10 +0600

    Webpack now bundling all our app code - We use import/export to indicate dependencies - Webpack makes sure everything loads in the correct order - We can remove all our additional script tags in index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert("So far so good");
+import { AlertService } from './app/alert.service';
+import { run } from './app/app';
+import { ComponentService } from './app/component.service';
+
+
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+
+run(alertService, componentService);
