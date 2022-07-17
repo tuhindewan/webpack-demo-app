e3495131445e8f7c34bbcae539f5147db44a3024
tuhindewan <tuhinsshadow@gmail.com>
2022-07-17 11:10:28 +0600

    Add Sass loader, override bootstrap colors - Changed our main.css to main.scss - Installed bootstrap locally so we can tweak it - Installed sass-loader and node-sass - Updated webpack.config file to include a .scss rule

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { AlertService } from './app/alert.service';
 import { run } from './app/app';
 import { ComponentService } from './app/component.service';
-import "./main.css";
+import "./main.scss";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,17 @@ module.exports = {
     },
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
+            {
+                test: /\.scss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ],
       },
 }
